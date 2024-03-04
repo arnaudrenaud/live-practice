cac5db98afbe9b85548417d0a78e420224dfff32
Arnaud Renaud <arnaud.renaud@gmail.com>
2024-03-04 14:16:47 +0100

    Render component `Temperature` with prop `value`

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Temperature />
+        <Temperature value={10} />
       </header>
     </div>
   );
